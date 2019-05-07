260c175d0cd1f0518491dc6c8270487cbbdc3933
Erin Ponsonby <erinponsonby@gmail.com>
2019-05-07 00:36:57 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    final_array = []
+    final_array = self.split(/[?!.]/)
+    final_array.delete_if {|sentence| sentence == "" }
+    final_array.count
   end
 end
