99478c7c416de7a4f1c6836892996bf29ad0aa0d
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-05-08 02:22:23 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,30 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true 
+    end
+    return false 
   end
 
   def question?
-
+    if self.end_with?('?')
+      return true 
+    end 
+    return false
   end
 
   def exclamation?
-
+    if self.end_with?('!')
+      return true 
+    end 
+    return false
   end
 
   def count_sentences
-
+    delimiters = ['. ', "! ", "? "]
+    words = self.split(Regexp.union(delimiters))
+    counts = words.count 
+    return counts 
   end
 end
