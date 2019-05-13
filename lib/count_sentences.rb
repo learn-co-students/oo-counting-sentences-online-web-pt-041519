7f10153ec587ee94d0d4d4b01156f77c6fa8ff59
zhenggao cai <caizhengao1992@gmail.com>
2019-05-13 04:07:22 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+     self.end_with?("?")
   end
 
   def exclamation?
-
+self.end_with?("!")
   end
 
   def count_sentences
-
-  end
-end
+    new_array = []
+    if self.size == 0 
+      return 0 
+      else
+     new_array = self.split(/\?|\.|!/)
+      n = 0 
+      count = 0 
+    for n in 0..new_array.size - 1 
+      if new_array[n].size == 0 
+       count += 1 
+    end
+     
+    end
+        return new_array.size - count
+        end
+      end
+    end
