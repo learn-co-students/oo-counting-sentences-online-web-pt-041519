ccb67a359659a79ef301aee51274df92a61c47b8
Caleb Aviles <earthctzn1@gmail.com>
2019-05-14 02:39:07 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+     self.end_with?("!")
   end
 
   def count_sentences
-
+    counter = 0 
+    x = self.split(/\.+|\?|!+/)
+    while counter < x.length do 
+      if sentence?
+        counter += 1 
+      elsif question?
+      counter += 1 
+      elsif exclamation?
+        counter += 1 
+      end
+    end
+    return counter
   end
 end
