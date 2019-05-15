5001c3f8dd287468022ca39e651589304f2e971c
Conor Owens <conorj.owens@gmail.com>
2019-05-15 03:07:56 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[-1] == "."
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self[-1] == "?"
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self[-1] == "!"
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+   sentences = self.split(/[.?!]/)
+   sentences.delete_if{|x| x.empty?}
+   sentences.length
   end
 end
