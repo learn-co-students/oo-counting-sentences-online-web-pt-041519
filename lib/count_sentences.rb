e7ac1f2b083fba11f61542dfac24f4b877a58923
Caleb Aviles <earthctzn1@gmail.com>
2019-05-15 04:53:46 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -13,19 +13,25 @@ class String
   def exclamation?
      self.end_with?("!")
   end
-
+  
   def count_sentences
-    counter = 0 
-    x = self.split(/\.+|\?|!+/)
-    while counter < x.length do 
-      if sentence?
-        counter += 1 
-      elsif question?
-      counter += 1 
-      elsif exclamation?
-        counter += 1 
-      end
-    end
-    return counter
+    input_string = self.split(/[.+?+!+]/).reject { |string| string.empty? }
+    input_string.length
   end
 end
+
+#  def count_sentences
+#    counter = 0 
+#    x = self.split(/\.+|\?|!+/)
+#    while counter < x.length do 
+#      if sentence?
+#        counter += 1 
+#      elsif question?
+#      counter += 1 
+#      elsif exclamation?
+#        counter += 1 
+#      end
+#    end
+#    return counter
+#  end
+#end
